e31eb3b8c13cb8266fddb0b37663b60d3f81adf0
himjjong <jjijji5728@kpu.ac.kr>
2022-07-23 23:20:08 +0900

    feat: add rq class

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -11,18 +11,18 @@ import java.io.IOException;
 public class GugudanServlet extends HelloServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8"); //들어오는 데이터를 UTF-8로 해석할것임
-        resp.setCharacterEncoding("UTF-8"); // 완성되는 html의 인코딩을 UTF-8로 할것
-        resp.setContentType("text/html; charset=utf-8");
-
-        int num = Integer.parseInt(req.getParameter("num"));
-        int limit = Integer.parseInt(req.getParameter("limit"));
 
 
-        resp.getWriter().append("<h1>구구단은 %d단 입니다!</h1>".formatted(num));
+        Rq rq = new Rq(req, resp);
+
+        int num = rq.getIntParam("num",0);
+        int limit = rq.getIntParam("limit",0);
+
+
+        rq.appendBody("<h1>구구단은 %d단 입니다!</h1>".formatted(num));
 
         for (int i = 1; i <= limit; i++) {
-            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(num, i, num * i));
+            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(num, i, num * i));
         }
     }
 }
