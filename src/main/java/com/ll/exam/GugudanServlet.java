d5290dc8d67e6589cfcba891995c1987dbb1a6c8
himjjong <jjijji5728@kpu.ac.kr>
2022-07-23 22:58:16 +0900

    feat: add 동적으로 만드는 구구단

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -1,23 +1,28 @@
 package com.ll.exam;
 
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HelloServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8"); //들어오는 데이터를 UTF-8로 해석할것임
         resp.setCharacterEncoding("UTF-8"); // 완성되는 html의 인코딩을 UTF-8로 할것
         resp.setContentType("text/html; charset=utf-8");
 
-        resp.getWriter().append("hello tomcat!!!!");
+        int num = Integer.parseInt(req.getParameter("num"));
+        int limit = Integer.parseInt(req.getParameter("limit"));
 
+
+        resp.getWriter().append("<h1>구구단은 %d단 입니다!</h1>".formatted(num));
+
+        for (int i = 1; i <= limit; i++) {
+            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(num, i, num * i));
+        }
     }
 }
