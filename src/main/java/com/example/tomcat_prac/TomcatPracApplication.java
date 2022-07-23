625e2ff6d946ea11c4696d212ac060684c124859
himjjong <jjijji5728@kpu.ac.kr>
2022-07-23 22:02:09 +0900

    settings

---
+++ src/main/java/com/example/tomcat_prac/TomcatPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomcat_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tomcat_prac/TomcatPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomcat_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
